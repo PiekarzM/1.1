d87516baa4731e4367bc14da177b53a3665e84b6
PiekarzM <127319430+PiekarzM@users.noreply.github.com>
2023-03-22 13:48:58 +0000

    Zmieniłem kolejność wyświetlania

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args){
+    String str = "Hello World";
+    for (int i = 0; i <3; i++)
+    {
+      for (int j = 0; j< str.length(); j++)
+      {
+        System.out.println(str.charAt(j));
+        
+      }
+    }
+    System.out.println(" ");
   }
 }
